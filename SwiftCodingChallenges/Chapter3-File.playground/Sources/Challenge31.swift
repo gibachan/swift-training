609d79a9b6c8c9628522bc88db141f5679af653a
gibachan <childhoodend.tk@gmail.com>
2018-01-13 09:07:41 +0900

    Add some fixes for challenge 31

--- SwiftCodingChallenges/Chapter3-File.playground/Sources/Challenge31.swift
+++ SwiftCodingChallenges/Chapter3-File.playground/Sources/Challenge31.swift
@@ -9,4 +9,24 @@ import Foundation
 // false if the copy failed, or the user specified something other than a directory.
 // -----------------------------------------------------------------------------
 
+public func challenge31(source: String, destination: String) -> Bool {
+    let fm = FileManager.default
+    var isDirectory: ObjCBool = false
+    
+    guard source != destination else { return false }
+    guard fm.fileExists(atPath: source, isDirectory: &isDirectory) && isDirectory.boolValue == true else { return false }
+    
+    let sourceURL = URL(fileURLWithPath: source)
+    let destinationURL = URL(fileURLWithPath: destination)
+    
+    do {
+        try fm.copyItem(at: sourceURL, to: destinationURL)
+    } catch {
+        print("Copy failed: \(error.localizedDescription)")
+        print(source)
+        print(destination)
+        return false
+    }
+    return true
+}
 
--- SwiftCodingChallenges/Chapter3-File.playground/Sources/Tests.swift
+++ SwiftCodingChallenges/Chapter3-File.playground/Sources/Tests.swift
@@ -37,6 +37,51 @@ public func testAll() {
     deleteFile(tempPath(to: "b.jg"))
     deleteFile(tempPath(to: "c.jpeg"))
 
+    // challenge31
+    let dir31_1 = "\(NSTemporaryDirectory())dir31_1"
+    let dir31_2 = "\(dir31_1)/dir31_2"
+    let dir31_3 = "\(dir31_2)/dir31_3"
+    let file31_1 = "\(dir31_1)/file31_1.txt"
+    let file31_2 = "\(dir31_2)/file31_2.txt"
+    let file31_3 = "\(dir31_3)/file31_3.txt"
+    let dir31_out = "\(NSTemporaryDirectory())dir31_out"
+    let dir31_out_1 = "\(dir31_out)/dir31_1"
+    let dir31_out_2 = "\(dir31_out_1)/dir31_2"
+    let dir31_out_3 = "\(dir31_out_2)/dir31_3"
+//    let file31_out_1 = "\(dir31_out_1)/file31_1.txt"
+//    let file31_out_2 = "\(dir31_out_2)/file31_2.txt"
+//    let file31_out_3 = "\(dir31_out_3)/file31_3.txt"
+    
+    // Create files
+    if fileExists(dir31_1) { deleteFile(dir31_1) }
+    if fileExists(dir31_out) { deleteFile(dir31_out) }
+    
+    createDirectory(at: dir31_1)
+    createDirectory(at: dir31_2)
+    createDirectory(at: dir31_3)
+    createDirectory(at: dir31_out)
+    createFile(at: file31_1, contents: "")
+    createFile(at: file31_2, contents: "")
+    createFile(at: file31_3, contents: "")
+    
+    // Tests for false cases
+    assert(challenge31(source: "/abc", destination: NSTemporaryDirectory()) == false, failedComment(31))    // from is not directory
+    assert(challenge31(source: NSTemporaryDirectory(), destination: NSTemporaryDirectory()) == false, failedComment(31))    // from == to returns false
+    
+    // TODO: Test for true case
+    // This does not work for permission issue
+    //assert(challenge31(source: dir31_1, destination: "\(dir31_out)/dir31_1") == true, failedComment(31))
+    //assert(fileExists(dir31_out_1) == true, failedComment(31))
+    //assert(fileExists(dir31_out_2) == true, failedComment(31))
+    //assert(fileExists(dir31_out_3) == true, failedComment(31))
+    //assert(fileExists(file31_out_1) == true, failedComment(31))
+    //assert(fileExists(file31_out_2) == true, failedComment(31))
+    //assert(fileExists(file31_out_3) == true, failedComment(31))
+    
+    // Clean files
+    if fileExists(dir31_1) { deleteFile(dir31_1) }
+    if fileExists(dir31_out) { deleteFile(dir31_out) }
+
 }
 
 public func failedComment(_ number: Int) -> String {
